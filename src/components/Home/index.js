007405d74dffca009c949d119a0dc79d7c2818ed
Delfino-app <delfinoapp@gmail.com>
2021-01-27 18:56:22 +0000

    Adding New task

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,56 @@
 import './home.css';
 import Task from "../Task";
+import {useState} from "react";
 
 const Home = () => {
+
+    const [listTask,setListTask] = useState([
+        {task:'Learn React Components and Hoocks',done:false,id:1},
+        {task:'Make a To do App Witch React',done:false,id:2},
+        {task:'work in Sig-Copenfl Project Today',done:false,id:3}
+    ]);
+
+    const [newTask,setNewTask] = useState("");
+    const [idValue,setIdValue] = useState(4);
+
+    const handleValueInput = (e) => {
+
+       setNewTask(e.target.value);
+    }
+
+    const handleSubmitform = (e) => {
+
+        e.preventDefault();
+
+        const data = {task:newTask,id:idValue}
+
+        setListTask([...listTask,data]);
+
+        setNewTask("");
+
+        const newId = idValue + 1;
+        setIdValue(newId);
+    }
+
+    const handleTogleStatus = (item) => {
+
+        //Toogle done value (true || false)
+        item.done = !item.done;
+
+        const newList = listTask.filter(list => list.id !== item.id);
+
+        if(item.done){
+
+            //Put a task done in the top
+            newList.unshift(item);
+        }
+        else{
+
+            newList.push(item);
+        }
+
+        setListTask(newList);
+    }
 
     return(
 
@@ -9,12 +58,12 @@ const Home = () => {
             <div className="container">
                 <h2><b>Things To Do</b></h2>
                 <div className="AddItemContent">
-                    <form method="POST" className="frmPost">
-                        <input type="text" placeholder="New Task..." />
+                    <form method="POST" className="frmPost" onSubmit={(e) => handleSubmitform(e)}>
+                        <input type="text" placeholder="New Task..." required value={newTask} onChange = {(e) => handleValueInput(e)} />
                         <button type="submit">Save</button>
                     </form>
                 </div>
-                <Task />
+                <Task  listTask ={listTask}  handleTogleStatus ={handleTogleStatus}/>
             </div>
         </div>
     )
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,24 +1,5 @@
-import {useState} from "react";
 
-const Task = () => {
-
-    const [listTask,setListTask] = useState([
-        {task:'Learn React Components and Hoocks',done:false,id:1},
-        {task:'Make a To do App Witch React',done:false,id:2}
-    ]);
-
-    const handleTogleStatus = (item) => {
-
-        item.done = !item.done;
-
-        const newList = listTask.filter(list => list.id !== item.id);
-
-        newList.push(item);
-
-        newList.reverse();
-
-        setListTask(newList);
-    }
+const Task = ({listTask, handleTogleStatus}) => {
 
     return (
 
@@ -28,9 +9,7 @@ const Task = () => {
                 <div className={list.done ? 'list taskDone' : 'list'} key={list.id} onClick={() => handleTogleStatus(list)}>
                     <h3>{list.task}</h3>
                     <div className="ActionContent">
-                       <span>
-                           
-                       </span>
+                       <span>{list.done ? '✔' : ''}</span>
                     </div>
                 </div>
             ))}
